b9b159e300e2a26c02ef2bfc4eb92b1d9c5d2551
Heestroy-118 <a11308@naver.com>
2023-03-16 11:01:56 +0900

    Bank commit

---
+++ Bank/src/Account.java
@@ -0,0 +1,25 @@
+ public class Account {
+
+        private String accountNu; //계좌번호
+        private int balance; //잔고
+
+        public Account(String accountNu) {
+            this.accountNu = accountNu;
+        }
+        public Account(int balance){
+            this.balance=balance;
+    }
+        public int deposit(int money){
+            balance = balance + money;
+            return balance;
+        }//입금
+
+        public int withdraw(int money){
+            balance = balance - money;
+            return balance;
+        } //출금
+
+     public void ShowBalance(){
+         System.out.println(balance);
+     }
+}
---
+++ Bank/src/Account_function.java
@@ -0,0 +1,33 @@
+import java.lang.invoke.SwitchPoint;
+import java.util.Scanner;
+public class Account_function {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);// 스캐너 객체 생성
+        boolean existence = true; // 돈이 있는지 없는지 확인
+        int money;
+
+        Account account = new Account(351 - 48648 - 648648);
+
+        while (existence) {
+            System.out.println(" ");
+        }
+
+        System.out.println("--------------------------------------");
+        System.out.println("| 1. 예금 | 2. 출금 | 3. 잔고 | 4. 종료 |");
+        System.out.println("--------------------------------------");
+        System.out.println(" 선택 >");
+
+        int menu = sc.nextInt(); // int 형 입력 및 리턴
+
+        switch (menu){
+            case 1:
+                System.out.println("1. 예금액 > ");
+                money=sc.nextInt();
+                account.deposit(money);
+                System.out.println(money+"원을 입금합니다.");
+                break;
+            case 2:
+                System.out.println("2. 출금액 >");
+        }
+    }
+}
