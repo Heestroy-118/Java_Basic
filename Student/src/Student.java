1bfd43107b148b7bc29540ceb7707bb2541dd59a
Heestroy-118 <a11308@naver.com>
2023-03-19 22:09:29 +0900

    commit

---
+++ Student/src/Student.java
@@ -0,0 +1,9 @@
+import javax.security.auth.Subject;
+import java.util.ArrayList-
+public class Student {
+    int StudentID;
+    String studentName;
+    ArrayList<Subject> subjectList; //AttayList 선언하기
+
+    public Student
+ }
