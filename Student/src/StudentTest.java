d8e8981655423dcd1d87056d6a0c73e549235ce4
Heestroy-118 <a11308@naver.com>
2023-03-20 14:06:13 +0900

    commit

--- Student/src/StudentTest.java
+++ Student/src/StudentTest.java
@@ -1,6 +1,16 @@
 public class StudentTest {
     public static void main(String[] args) {
-        Student studentLee = new Student(1001,Lee);
+        Student studentLee = new Student(1001,"Lee");
+        studentLee.addSubject("국어",80);
+        studentLee.addSubject("수학", 100);
 
+        Student studentkim = new Student(1002, "Kim");
+        studentkim.addSubject("국어", 90);
+        studentkim.addSubject("수학", 90);
+        studentkim.addSubject("영어", 100);
+
+        studentLee.showStudentINfo();
+        System.out.println("===========================");
+        studentkim.showStudentINfo();
     }
 }
